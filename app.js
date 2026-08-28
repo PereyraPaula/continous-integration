bf64cc9ab61fbcad3bb318cf536b9c9ee02d6a8c
Paula Pereyra <65372882+PereyraPaula@users.noreply.github.com>
2026-08-27 23:56:32 -0300

    format and linter work fine

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    let variable = 'Works on my machine.'
+    const variable = 'Works on my machine.';
     res.send(variable);
 });
 
